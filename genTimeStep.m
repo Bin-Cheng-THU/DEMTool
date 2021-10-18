226ec51d699e66b7a9af2036801a8f6d262bccf3
Bin-Cheng-THU <chengbin171@163.com>
2021-10-18 13:32:15 +0800

    DEMTool 1.5
    add packing model and sound wave model

--- genTimeStep.m
+++ genTimeStep.m
@@ -6,9 +6,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc;clear
 format long;
-%% Input parameters
+%% Input parameters for collision
 global E Rho nu v r e m beta
-E = 1e8; % Young's Modules
+E = 1e9; % Young's Modules
 Rho = 3.2; % Density
 nu = 0.30; % Poisson's ratio
 v = 10; % maximum velocity
@@ -16,7 +16,13 @@ r = 0.3; % average radius
 e = 0.2; % coefficient of restitution
 m = 4/3*pi*r^3*Rho; % particles mass
 beta = 0.6; % rolling properties
+%% Input parameters for packing
+Phi = 0.65; % packing ratio
+H = 40; % Height of the granular bed
+g = 980; % gravity
 %% Estimate time step, model 1
+fprintf('*********************************\n');
+fprintf('碰撞模型估计：\n');
 T = 5.84*(Rho*(1-nu^2)/E)^(2/5)*r*v^(-1/5);
 dT = T/20;
 fprintf('模型一估计时间步长为  %d\n', dT);
@@ -47,3 +53,25 @@ fprintf('实际恢复系数  %f\n', CoR);
 fprintf('实际接触时间  %d\n', Time(PosFlag+TimeFlag));
 fprintf('实际估计步长  %d\n', Time(PosFlag+TimeFlag)/20);
 fprintf('MATLAB计算步长  %d\n', diff(Time(1:2)));
+%% Packing check
+fprintf('*********************************\n');
+fprintf('堆积模型估计：\n');
+force = Rho*Phi*g*H*pi*r^2;
+BottomeOverlap = (force*3*(1-nu^2)/2/E/sqrt(r))^(2/3);
+fprintf('最大重叠量  %.4f%%\n', BottomeOverlap*100);
+%% Sound velocity check
+fprintf('*********************************\n');
+fprintf('声速模型估计：\n');
+Velocity = 2*r/T;
+fprintf('颗粒声速为  %d\n', Velocity);
+%% Rayleigh wave check
+fprintf('*********************************\n');
+fprintf('瑞利波模型估计：\n');
+dT = pi*(r/(0.163*nu+0.877)*sqrt(Rho/E));
+fprintf('瑞利波模型估计时间步长为  %d\n', dT);
+
+
+
+
+
+
